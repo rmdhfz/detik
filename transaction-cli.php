2c48c3d9386723a2f12d74d5b4c9a34d3cc13e71
Hafiz Ramadhan <hfzrmd@gmail.com>
2021-12-08 07:42:10 +0700

    Add: validation status in transaction-cli.php

--- transaction-cli.php
+++ transaction-cli.php
@@ -1,18 +1,22 @@
 <?php
 
 $reference_id = stripcslashes(htmlspecialchars(htmlentities($argv[1])));
-$status = stripcslashes(htmlspecialchars(htmlentities($argv[2])));
+$status = strtolower(stripcslashes(htmlspecialchars(htmlentities($argv[2]))));
 if ($reference_id && $status) {
 	include 'db.php';
 	$check = $db->query("SELECT id FROM transaction WHERE reference_id = '$reference_id'");
 	if ($check->num_rows > 0) {
-		$update = $db->query("UPDATE transaction SET status_transaction = '$status' WHERE reference_id = '$reference_id'");
-		if ((bool) $update) {
-			echo 'Sucessful update status transaction';
+		if ($status === "paid" || $status === "failed") {
+			$update = $db->query("UPDATE transaction SET status_transaction = '$status' WHERE reference_id = '$reference_id'");
+			if ((bool) $update) {
+				echo 'Sucessful update status transaction';
+			}else{
+				echo 'Failed update status transaction';
+			}
+			$db->close();
 		}else{
-			echo 'Failed update status transaction';
+			echo 'unknown status';
 		}
-		$db->close();
 	}else{
 		echo 'transaction not found';
 	}
